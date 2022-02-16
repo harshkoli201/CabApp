b5e73d2f8d982e8c345b4e0906ebb28f6f283b85
Harsh Koli <harshkoli201@gmail.com>
2022-02-16 19:36:42 +0530

    Basic

---
+++ app1/lib/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Mybutton(),
+        ),
+      ),
+    ),
+  );
+}
+
+class Mybutton extends StatelessWidget {
+  const Mybutton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('My Button has been Clicked');
+      },
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.blue[500],
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
