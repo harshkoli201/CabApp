24f41dac37f12e243d0b51883a8d63e31e0b8b95
Harsh Koli <harshkoli201@gmail.com>
2022-06-15 01:34:23 +0530

    SignUp

--- ola/lib/MainScreen/signUp.dart
+++ ola/lib/MainScreen/signUp.dart
@@ -41,124 +41,27 @@ class _SignUpState extends State<SignUp> {
           height: MediaQuery.of(context).size.height,
           width: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: const <Widget>[
-                        Text(
-                          "Sign up",
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.yellow),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "Create an Account, it's Free",
-                          style: TextStyle(fontSize: 15, color: Colors.grey),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
-                        children: <Widget>[
-                          makeInput(label: "Email"),
-                          makeInput(label: "Password", obscureText: true),
-                          makeInput(
-                              label: "Confirm Password", obscureText: true),
-                        ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: const <Widget>[
+                      Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.yellow),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40),
-                      child: Container(
-                        child: MaterialButton(
-                          minWidth: double.infinity,
-                          height: 60,
-                          onPressed: () {},
-                          shape: RoundedRectangleBorder(
-                              side: const BorderSide(color: Colors.yellow),
-                              borderRadius: BorderRadius.circular(50)),
-                          child: const Text(
-                            "Sign up",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          "Already have an account?",
-                          style: TextStyle(color: Colors.yellow),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (constext) => const LoginPage()),
-                            );
-                          },
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(
-                                color: Colors.yellow,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 18),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-
-  Widget makeInput({label, obscureText = false}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          label,
-          style: const TextStyle(
-              fontSize: 15, color: Colors.yellow, fontWeight: FontWeight.w400),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        TextField(
-          obscureText: obscureText,
-          decoration: const InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey),
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-      ],
     );
   }
 }
